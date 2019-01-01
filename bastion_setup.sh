304f9391017a468971c40c092ab4c5e8403c87a6
Hiroshi Kobayashi <hiroshi.kobayashi@wdc.com>
2019-01-02 04:13:57 +0900

    added "-y" option for yum-config

--- bastion_setup.sh
+++ bastion_setup.sh
@@ -17,8 +17,9 @@ sudo pip install awscli
 
 # install docker-ce
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo -y
 sudo yum install docker-ce -y
+sudo systemctl enable docker
 sudo systemctl start docker
 
 # install packer
