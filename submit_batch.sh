573fc8e274382569b30bb4a8345ca1cf2fee059e
Hiroshi Kobayashi <hiroshi.kobayashi@wdc.com>
2019-01-04 02:20:11 +0900

    gpu job submittion script, initial commit

---
+++ submit_batch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+QUEUE="gpu-jobqueue"
+JOBDEF="gpu-job"
+REGION="us-west-2"
+TIMES=10
+
+for i in `seq 1 $TIMES`
+do
+  JOBNAME="JOB_$i"
+  aws --region $REGION batch submit-job --job-name $JOBNAME --job-queue $QUEUE --job-definition $JOBDEF
+  echo "submitted $JOBNAME"
+  sleep 10
+done
